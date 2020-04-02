f355c2779034da1fffeb21d39278e8b6fcdde0db
Michal Stankay <michal@stankay.net>
2020-04-02 14:26:12 +0200

    Remote addr filtering

---
+++ src/main/java/test/SayHelloRestController.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SayHelloRestController {
+
+    @GetMapping("/sayhello")
+    public String get() {
+        return "hello\n";
+    }
+}
