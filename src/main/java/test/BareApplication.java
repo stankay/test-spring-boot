50b2badc5e9a8fc25d67ab938a15db0eca8577ad
Michal Stankay <michal@stankay.net>
2022-01-31 19:37:38 +0100

    Rename main class to BareApplication

---
+++ src/main/java/test/BareApplication.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+@EnableAsync
+@EnableScheduling
+public class BareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BareApplication.class, args);
+    }
+}
