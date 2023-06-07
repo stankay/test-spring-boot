484d6742809452adadb53d93344ad872a5eecc0c
Michal Stankay <michal@stankay.net>
2023-06-07 09:19:07 +0200

    Experiments with quartz scheduler

--- src/main/java/bare/camel/CamelRouteBuilder.java
+++ src/main/java/bare/camel/CamelRouteBuilder.java
@@ -1,8 +1,9 @@
-package camel.fail;
+package bare.camel;
 
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,9 @@ public class CamelRouteBuilder extends RouteBuilder {
     @Autowired
     ProducerTemplate producerTemplate;
 
+    @Value("${cron.expression}")
+    String cronExpression;
+
     private static final String DIRECT_FOO = "direct:foo";
 
     @Override
@@ -21,6 +25,12 @@ public class CamelRouteBuilder extends RouteBuilder {
         .id(DIRECT_FOO)
         .log(body().toString())
         ;
+
+        if (!cronExpression.trim().equals("-")) {
+            from("quartz://stankayTimer?cron=" + cronExpression)
+                    .id("quartzTimerRoute")
+                    .log("ahoj");
+        }
     }
 
     @EventListener
